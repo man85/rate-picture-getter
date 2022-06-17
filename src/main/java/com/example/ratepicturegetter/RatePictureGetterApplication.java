bec62b737f9c74ec2faadbec6fc3ce6231797491
man85 <man85@yandex.ru>
2022-06-17 14:40:38 +0300

    Initialize commit

---
+++ src/main/java/com/example/ratepicturegetter/RatePictureGetterApplication.java
@@ -0,0 +1,13 @@
+package com.example.ratepicturegetter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatePictureGetterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RatePictureGetterApplication.class, args);
+    }
+
+}
